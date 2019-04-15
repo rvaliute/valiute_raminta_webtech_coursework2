ca0bbbe2e89d77a8d3967db311d0768824441621
rvaliute <46711750+rvaliute@users.noreply.github.com>
2019-04-15 16:17:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ var path = require('path');
 var dbpath = path.resolve(__dirname, './LoginDetails.db')
 var sqlite3 = require('sqlite3').verbose(); //npm install sqlite3 into nodejs
 
-/*
-var db = new sqlite3.Database('helloapp\users.db', (err) => {
-	if (err) {
-		console.error(err.message);
-	}
-	console.log('connected to users database');
-});
-
-/*db.serialize(function() {
-	db.run("INSERT INTO  users (ID, username, password) VALUES (?, ?)", "helloapp", 0);
-});
-*/
-
 var express = require('express');
 var router = express.Router();
 var userapp = express();
@@ -34,31 +21,30 @@ router.post('/', function(req, res) {
 	var username = req.body.uname;
 	var password = req.body.upass;
 	
-	var today = new Date();
-	var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
-
-	function setCookie(name, value)
-	{
-		document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
-	}
-
-	function putCookie(form)
-    //this should set the UserName cookie to the proper value;
-	{
-		setCookie("userName", form[0].username.value);
-		return true;
-	}
+	function authenticate(username, password, cb){
+	var exists = false;
+	//check against db
+	db.get(select * from users where username = '"+username+"' AND password='"+password+", function(err,row){
 	
-	res.render('profileLanding', {title: username}); //render name of user logged in
-});
-
-router.get('/success', function(req, res){
-	res.redirect('profile');
-});
-
-router.get('/profile', function(req, res){
-	//render the profile page. all users will use the same pug.
-	//also remder the same messages page
+	if(err)console.log(err);
+	
+	if(typeof row == "undefined"){
+		exists = false;
+		console.log("Invalid username or password.");
+		cb(exists);
+	} else {
+		exists = true;
+		console.log("Success.");
+		cb(exists);
+		res.render('profileLanding', {title: username}); //render name of user logged in
+		}
 	});
+});
+				
+						
+
+router.get('/messages', function(req, res) {
+	res.render('messages', {title: "Inbox"});
+});
 	
 module.exports = router;
